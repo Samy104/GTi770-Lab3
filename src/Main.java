2088a2f58695f8d7a951e8e9f16114800f659c14
Étienne Faucher <et.faucher@gmail.com>
2014-06-06 10:05:43 -0400

    Voici le départ du projet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,112 @@
 
-public class Main {
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import Jama.Matrix;
+
+public class Main {
+	
+	private static String fileString = "data.txt";
+	private static int numberDataByLine = 6;
+	
+	public static void main(String[] args) {
+		Matrix matrix = ReadData();
+		//matrix.print(0, 6);
+		
+		Matrix class1 = SplitMatrices(matrix, true);
+		Matrix class2 = SplitMatrices(matrix, false);
+		//class1.print(0, 6);
+		//class2.print(0, 6);
+	}
+	
+	public static Matrix ReadData()
+	{
+		Matrix matrix = null;
+		try
+		{
+			String lineData = "";
+			BufferedReader br = new BufferedReader(new FileReader(fileString));
+		    try 
+		    {
+		        StringBuilder sb = new StringBuilder();
+		        String line = br.readLine();
+
+		        while (line != null) 
+		        {
+		            sb.append(line);
+		            //sb.append(System.lineSeparator());
+		            sb.append(" ");
+		            line = br.readLine();
+		        }
+		        lineData = sb.toString();
+		    } 
+		    catch(Exception ex)
+		    {
+		    	System.out.println(ex.getMessage());
+		    }
+		    finally 
+		    {
+		        br.close();
+		    }
+		    
+		    if(lineData != "")
+		    {
+		    	String[] data = lineData.split(" ");
+		    	double[][] datas = new double[data.length/numberDataByLine][numberDataByLine];
+		    	for(int i = 0; i < data.length/numberDataByLine; i++)
+		    	{
+		    		for(int j = 0; j < numberDataByLine; j++)
+		    		{
+		    			datas[i][j] = Double.parseDouble(data[i * numberDataByLine + j]);
+		    		}
+		    	}
+		    	
+		    	matrix = new Matrix(datas, data.length/numberDataByLine, numberDataByLine);
+		    }
+		    
+		}
+		catch (IOException exception)
+		{
+			
+		}
+		
+		return matrix;
+	}
+
+	public static Matrix SplitMatrices(Matrix originalMatrix, boolean getClassOne)
+	{
+		int rowIndex = -1;
+		double newClass = 2d;
+		Matrix matrix = null;
+		
+		
+		for(int i = 0; i < originalMatrix.getRowDimension(); i++)
+		{
+			if(originalMatrix.get(i, 0) == newClass)
+			{
+				rowIndex = i;
+				break;
+			}
+		}
+		
+		if(rowIndex != -1)
+		{
+			if(getClassOne)
+			{
+				matrix = originalMatrix.getMatrix(0, rowIndex-1, 0, numberDataByLine-1);
+				return matrix;
+			}
+			else
+			{
+				matrix = originalMatrix.getMatrix(rowIndex, originalMatrix.getRowDimension() - 1, 0, numberDataByLine-1);
+				return matrix;
+			}
+		}
+		else
+		{
+			System.out.println("Can't find two classes !");
+			return null;
+		}
+	}
 }
